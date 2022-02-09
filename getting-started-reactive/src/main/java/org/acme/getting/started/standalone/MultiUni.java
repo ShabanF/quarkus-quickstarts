b7dd39ab9f7f839166a984da7a81488d56784499
Shaban Hyseni <shabanhyseni@kodelabs.com>
2022-02-09 17:07:22 +0100

    adds vertex example project

--- getting-started-reactive/src/main/java/org/acme/getting/started/standalone/MultiUni.java
+++ getting-started-reactive/src/main/java/org/acme/getting/started/standalone/MultiUni.java
@@ -1,16 +1,24 @@
 package org.acme.getting.started.standalone;
 
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MultiUni {
 
     public static void main(String[] args) {
 
-        invoke();
+        //invokeUni();
+        //subscribe();
 
+        //subscribeMulti();
+
+        mapAndFlatMap();
     }
 
-    private static void invoke() {
+    private static void invokeUni() {
         Uni<String> uni = Uni.createFrom()
                 .<String>emitter(emitter ->
                         new Thread(() -> emitter.complete("hello from " + Thread.currentThread().getName()))
@@ -20,5 +28,48 @@ public class MultiUni {
         uni.onItem().invoke(item -> {
             System.out.println("Received item " + item);
         }).await().indefinitely();
+    }
+
+    private static void subscribe() {
+        Uni<String> helloUni = Uni.createFrom()
+                .item("hello")
+                .onItem().transform(item -> item + " mutiny")
+                .onItem().transform(String::toUpperCase);
+
+        helloUni.onSubscription().invoke(() -> System.out.println()).subscribe().with(item -> System.out.println(item));
+
+    }
+
+    private static void subscribeMulti() {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        Multi<List<String>> multi = Multi.createFrom().item(list);
+
+        multi.onSubscription().invoke(items -> {
+                    items.request(0);
+                })
+                .onCompletion().invoke(() -> {
+                    System.out.println("On completion invoked");
+                }).subscribe().with(strings -> {
+                    System.out.println(strings);
+                });
+    }
+
+    private static void mapAndFlatMap() {
+
+        Uni<String> uni = Uni.createFrom().item("hello");
+
+        Uni<String> hello2 = uni.flatMap(s -> {
+
+            Uni<String> s1 = Uni.createFrom().item(s.concat("hello2"));
+
+            return s1;
+        });
+
+        hello2.subscribe().with(System.out::println);
+
     }
 }
