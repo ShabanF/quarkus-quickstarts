4bd4ffc2c9b36eed7f604b9d801ad4156aee38fb
Shaban Hyseni <shabanhyseni@kodelabs.com>
2022-02-09 13:58:13 +0100

    removes unused code fragments

--- getting-started-reactive/src/main/java/org/acme/getting/started/service/PersonService.java
+++ getting-started-reactive/src/main/java/org/acme/getting/started/service/PersonService.java
@@ -104,11 +104,6 @@ public class PersonService {
             return PERSONS;
         });
 
-        Uni<Person> invoke = personUni.onItem()
-                .invoke(i -> {
-                    System.out.println("Received item: " + i);
-                });
-
         return allPersons;
     }
 
